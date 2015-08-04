e050d1108baa24e88ac56a36c1f698ce1fde295f
Milton Madanda <madandat@gmail.com>
2015-08-04 15:33:07 +0200

    generate non minified css

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ virtualenv ve
 source ve/bin/activate
 echo 'Starting webserver..'
 pserve development.ini --reload &
-sass --watch unicorecmsa4w/static/sass:unicorecmsa4w/static/css --style compressed 
+sass --watch unicorecmsa4w/static/sass:unicorecmsa4w/static/css
 kill %1
 echo 'done'
